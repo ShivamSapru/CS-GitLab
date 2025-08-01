c8ffcc2a6b21302865507863bcf1350682ba9128
srlimkar29 <srlimkar29@gmail.com>
2025-08-01 23:36:03 +0100

    Secirity - Keys are now not exposed & UI - Overlay trasition is reduced

---
+++ chrome-extension/background/config.js
@@ -0,0 +1,6 @@
+// config.js
+export const CONFIG = {
+  AZURE_TRANSLATOR_KEY: "",
+  AZURE_TRANSLATOR_REGION: "",
+  AZURE_TRANSLATOR_ENDPOINT: "https://translator-api-mgmt.azure-api.net/translator"
+};
