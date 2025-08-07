087257fe3bf4c8c0db78b9b040ba026651fd731a
Shubham Limkar <77462500+srlimkar29@users.noreply.github.com>
2025-08-07 17:06:02 +0100

    Delete chrome-extension directory

--- chrome-extension/background/config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-export const CONFIG = {
-  AZURE_TRANSLATOR_KEY: "",
-  AZURE_TRANSLATOR_REGION: "",
-  AZURE_TRANSLATOR_ENDPOINT: "https://translator-api-mgmt.azure-api.net/translator"
-};
